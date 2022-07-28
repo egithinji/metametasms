dc098352c4d8119431b715e0c5c51e58b3f1f006
Eric Githinji <ericgithinji@gmail.com>
2022-07-28 17:52:15 +0300

    Upload Readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the sms sending module of the system developed by the metameta team at the Africa's Talking hackathon of July 28th 2022.
+
+It provides an enpoint `send_message` that receives two parameters: a destination telephone number and a message.
+
+It then uses Africa's Talking's api to send a message to the destination telephone number, informing them of the status of their application.
--- message_server.js
+++ message_server.js
@@ -8,16 +8,14 @@ app.get("/", function(req,res){
 });
 
 app.post("/send_message", function(req,res){
-    //console.log("received a send_message post request");
+    console.log("received a send_message post request");
     messageSender(req.body.tel_num, req.body.message);
-    //console.log("sent a message");
+    console.log("sent a message");
     res.status(200);
 });
 
-//var server = app.listen(8000, function () {
-    //var host = server.address().address;
-    //var port = server.address().port;
-    //console.log("Server listening at http://%s:%s",host,port);
-//})
-
-app.listen(5000);
+var server = app.listen(8000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Server listening at http://%s:%s",host,port);
+})
