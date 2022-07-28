afcce02ca02ba7ca7294b503ce0079c2cdd46fd7
Eric Githinji <ericgithinji@gmail.com>
2022-07-28 16:17:42 +0300

    Comment out console messages.

--- sendSMS.js
+++ sendSMS.js
@@ -1,23 +1,19 @@
 const AfricasTalking = require('africastalking');
 require('dotenv').config();
-// TODO: Initialize Africa's Talking
 const africastalking = AfricasTalking({
   apiKey: process.env.API_KEY, 
-  username: 'sandbox'
+  username: 'metameta'
 });
 
-
 module.exports = async function sendSMS(tel_num,message) {
-    
-    // TODO: Send message
     try {
         const result=await africastalking.SMS.send({
             to: tel_num, 
             message: message,
-            from: 'METAMETA'
+            from: 'AT'
         });
-        console.log(result);
+        //console.log(result);
     } catch(ex) {
-        console.error(ex);
+        //console.error(ex);
     } 
 };
